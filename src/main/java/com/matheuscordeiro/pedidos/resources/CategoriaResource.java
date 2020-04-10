86dab520ffec018c37e8f420f0d9e97baa1678a7
Vanittas <matheus05paula@gmail.com>
2020-04-10 02:02:18 -0300

    (DONE) Teste de classe de domínio inicial

--- src/main/java/com/matheuscordeiro/pedidos/resources/CategoriaResource.java
+++ src/main/java/com/matheuscordeiro/pedidos/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.matheuscordeiro.pedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheuscordeiro.pedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "GetMapping ok";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1, "informática");
+		Categoria categoria2 = new Categoria(2, "escritório");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(categoria1);
+		listaCategoria.add(categoria2);
+		
+		return listaCategoria;
+		
 	}
 }
