9a36b67f22055391cc33041d2934f0c9617f37bb
Vanittas <matheus05paula@gmail.com>
2020-04-10 16:19:09 -0300

    (DONE) criação da classe de domínio Produto e associação ManyToMany no atributo categorias

--- src/main/java/com/matheuscordeiro/pedidos/PedidosApplication.java
+++ src/main/java/com/matheuscordeiro/pedidos/PedidosApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.matheuscordeiro.pedidos.domain.Categoria;
+import com.matheuscordeiro.pedidos.domain.Produto;
 import com.matheuscordeiro.pedidos.repositories.CategoriaRepository;
+import com.matheuscordeiro.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PedidosApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -24,7 +28,20 @@ public class PedidosApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria categoria1 = new Categoria(null, "informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
+				
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 	}
 
 }
