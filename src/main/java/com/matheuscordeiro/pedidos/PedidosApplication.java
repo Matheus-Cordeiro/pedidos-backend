e8c307f5c2313dac69835d113a8152d2fa2bf30a
Vanittas <matheus05paula@gmail.com>
2020-04-10 15:26:45 -0300

    (DONE) Criação do método que instancia e salva objetos de Categoria automáticamente ao rodar o projeto

--- src/main/java/com/matheuscordeiro/pedidos/PedidosApplication.java
+++ src/main/java/com/matheuscordeiro/pedidos/PedidosApplication.java
@@ -1,13 +1,30 @@
 package com.matheuscordeiro.pedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PedidosApplication {
+import com.matheuscordeiro.pedidos.domain.Categoria;
+import com.matheuscordeiro.pedidos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PedidosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 	}
 
 }
