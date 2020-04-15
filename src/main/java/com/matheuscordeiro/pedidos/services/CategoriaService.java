0bd5b14c500ee1b8e1c076e44674914e5b79951b
Vanittas <matheus05paula@gmail.com>
2020-04-15 04:05:30 -0300

    (DONE) Páginação com parâmetros opcionais na requisição GET

--- src/main/java/com/matheuscordeiro/pedidos/services/CategoriaService.java
+++ src/main/java/com/matheuscordeiro/pedidos/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.matheuscordeiro.pedidos.domain.Categoria;
@@ -47,6 +50,11 @@ public class CategoriaService {
 		} catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("A exclusão de uma categoria que possui produtos atrelados a ela é proibida :c");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String direction, String orderBy){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 	
 }
